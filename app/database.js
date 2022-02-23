949ebcfb5a342635d953fc6ddb1c40387990a488
Marie Gueho <35000711+Marie-Gueho@users.noreply.github.com>
2022-02-23 20:41:49 +0100

    Update database.js

--- app/database.js
+++ app/database.js
@@ -1,6 +1,5 @@
-//const dotenv = require('dotenv');
 const { Client } = require('pg');
-//const pool = new Pool(process.env.PG_URL);
+
 const pool = new Client(process.env.PG_URL)
 
 pool.connect();
